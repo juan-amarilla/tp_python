bc6c65f1ec574b3055cc6eb210dcf215a324900d
juan amarilla <juanamarillasebastian@gmail.com>
2024-09-27 19:32:58 -0300

    firs commit

---
+++ TP_1.py
@@ -0,0 +1,38 @@
+import kiosco as kios
+from ingreso_validado import ingresar_y_validar_entero
+
+inventario = [
+    ["Chupetin sable de luz", 50, 200],
+    ["Agua de la fuerza", 35, 3200],
+    ["Gomitas holocubo", 25, 990],
+    ["Barrita de cereal wookiee", 40, 2500],
+    ["Galletitas R2D2", 20, 15800],
+]
+
+opcion = 0;
+mensaje = """
+1-Agregar producto al inventario
+2-Realizar una venta
+3-Mostrar productos disponibles
+4-Salir del sistema\n""";
+
+while opcion != 4:
+
+   opcion = ingresar_y_validar_entero(mensaje);
+
+   match opcion:
+
+        case 1:
+            estado = kios.agregar(inventario);
+        
+        case 2:
+            print(2);
+        
+        case 3:
+            estado = kios.mostrar_inventario(inventario);
+        
+        case 4:
+            print("Se salio exitosamente el programa gracias por usarlo. ");
+        
+        case _:
+            print("No");
